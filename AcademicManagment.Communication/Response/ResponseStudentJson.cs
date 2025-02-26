131da4fa075a6629f153cf5d1c31d0276184d9ae
Ranna <rannamead58@gmail.com>
2025-02-26 05:23:03 -0300

    fix responseJson

--- AcademicManagment.Communication/Response/ResponseStudentJson.cs
+++ AcademicManagment.Communication/Response/ResponseStudentJson.cs
@@ -2,7 +2,7 @@
 {
     public class ResponseStudentJson
     {
-        public Guid Id { get; set; } = string.Empty;
+        public Guid Id { get; set; } 
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime DateBirth { get; set; }
